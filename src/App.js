bbafed1877ada14c9745fc5a8d6179dc6903e768
Talha-Ali150 <118039339+Talha-Ali150@users.noreply.github.com>
2023-06-06 04:25:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import EventForm from './components/EventForm/EventForm';
+import TicketForm from './components/TicketForm/TicketForm';
 
 function App() {
   return (
     <div className="App">
-      <EventForm />
+      //<EventForm />
+    <TicketForm />
     </div>
   )
 }
